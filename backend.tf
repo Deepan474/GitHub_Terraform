08dbce01b3958396d87e445b6f4228cd876a5493
Deepan474 <dpan474@gmail.com>
2026-02-17 20:21:05 +0530

    Added backend config with details

--- backend.tf
+++ backend.tf
@@ -1,8 +1,15 @@
 terraform {
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+}
   backend "s3" {
     bucket = "statefile-deep"
     key    = "demo.tfstate"
     region = "us-east-1"
     use_lockfile = true
   }
+  required_version = "~> 1.10"
 }
