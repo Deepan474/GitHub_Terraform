dc5d7d28cefcf4fe09726ff925d815e49f98a1f3
Deepan474 <dpan474@gmail.com>
2026-02-26 17:35:47 +0530

    added custom VPC-1

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.My_VPC.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = us-east-1a
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "My-Public-Subnet"
